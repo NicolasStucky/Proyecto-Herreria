90d56a3720815fec89c9313b44a00e4eefc4242c
unknown <nicolastucky2005@gmail.com>
2023-11-29 22:12:52 -0300

    modelo

---
+++ back/api/api.js
@@ -0,0 +1,35 @@
+{
+	"trabajos"[
+	  {
+		"tipo": "portones",
+		"material": "metal",
+		"url": "https://tu-sitio-web.com/portones"
+	  },
+	  {
+		"tipo": "rejas",
+		"material": "metal",
+		"url": "https://tu-sitio-web.com/rejas"
+	  },
+	  {
+		"tipo": "cercas",
+		"material": "metal",
+		"url": "https://tu-sitio-web.com/cercas"
+	  },
+	  {
+		"tipo": "techos",
+		"material": "metal",
+		"url": "https://tu-sitio-web.com/techos"
+	  },
+	  {
+		"tipo": "puertas",
+		"material": "metal",
+		"url": "https://tu-sitio-web.com/puertas"
+	  },
+	  {
+		"tipo": "piso deck",
+		"material": "madera",
+		"url": "https://tu-sitio-web.com/piso-deck"
+	  }
+	]
+  }
+  
