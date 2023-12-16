a1c2b8a3179dc646c1ce25df5876708d4e77a35b
Massimo Damiano <106933868+MassimoDamiano@users.noreply.github.com>
2023-12-16 13:20:37 -0300

    Update App.jsx

--- front/proyecto-front/src/App.jsx
+++ front/proyecto-front/src/App.jsx
@@ -1,3 +1,4 @@
+import logo from './logo.svg';
 import './App.css';
 import Header from './Components/Header/Header';
 import Contacto from './Components/Contacto/Contacto';
@@ -5,7 +6,22 @@ function App() {
   return (
     <div className="App">
       <Header/>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn Reacttt
+        </a>
+      </header>
       <Contacto/>
+      
     </div>
   );
 }
